70d918d03f4d2bb06ce3d098966fe4cf52cdcffd
Evgeny Kuvshinov <41694637+jug2505@users.noreply.github.com>
2021-04-11 13:38:33 +0300

    Прототип

--- summary.py
+++ summary.py
@@ -4,11 +4,6 @@ from transformer import TextNormalizer
 import numpy as np
 import pandas as pd
 import nltk
-import re
-from bs4 import BeautifulSoup
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from nltk.corpus import stopwords
 from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Concatenate, TimeDistributed
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import EarlyStopping
@@ -17,9 +12,9 @@ import warnings
 pd.set_option("display.max_colwidth", 200)
 warnings.filterwarnings("ignore")
 
-data_train = pd.read_json("corpus/train.jsonl", lines=True)
-data_val = pd.read_json("corpus/val.jsonl", lines=True)
-data_test = pd.read_json("corpus/test.jsonl", lines=True)
+data_train = pd.read_json("corpus/train.jsonl", lines=True)[:100]
+data_val = pd.read_json("corpus/val.jsonl", lines=True)[:25]
+data_test = pd.read_json("corpus/test.jsonl", lines=True)[:10]
 
 data_train.info()
 data_val.info()
@@ -86,7 +81,7 @@ length_df.hist(bins=30)
 plt.show()
 
 max_summary_len = 50
-max_text_len = 800
+max_text_len = 400
 
 # Выбор предложений которые меньше максимума
 cleaned_train_text = np.array(data['cleaned_train_text'])
@@ -125,14 +120,15 @@ for i in range(len(cleaned_val_text)):
         val_text.append(cleaned_val_text[i])
         val_summary.append(cleaned_val_summary[i])
 
-x_train = np.array(pd.DataFrame({'p': train_text})['p'].apply(lambda x: 'SOL ' + x + ' EOL'))
-y_train = np.array(pd.DataFrame({'p': train_summary})['p'].apply(lambda x: 'SOL ' + x + ' EOL'))
+x_train = np.array(pd.DataFrame({'p': train_text})['p'].apply(lambda x: 'sol ' + x + ' eol'))
+y_train = np.array(pd.DataFrame({'p': train_summary})['p'].apply(lambda x: 'sol ' + x + ' eol'))
 
-x_val = np.array(pd.DataFrame({'p': val_text})['p'].apply(lambda x: 'SOL ' + x + ' EOL'))
-y_val = np.array(pd.DataFrame({'p': val_summary})['p'].apply(lambda x: 'SOL ' + x + ' EOL'))
 
-x_test = np.array(pd.DataFrame({'p': test_text})['p'].apply(lambda x: 'SOL ' + x + ' EOL'))
-y_test = np.array(pd.DataFrame({'p': test_summary})['p'].apply(lambda x: 'SOL ' + x + ' EOL'))
+x_val = np.array(pd.DataFrame({'p': val_text})['p'].apply(lambda x: 'sol ' + x + ' eol'))
+y_val = np.array(pd.DataFrame({'p': val_summary})['p'].apply(lambda x: 'sol ' + x + ' eol'))
+
+x_test = np.array(pd.DataFrame({'p': test_text})['p'].apply(lambda x: 'sol ' + x + ' eol'))
+y_test = np.array(pd.DataFrame({'p': test_summary})['p'].apply(lambda x: 'sol ' + x + ' eol'))
 
 # Токенизация
 from keras.preprocessing.text import Tokenizer
@@ -142,7 +138,7 @@ x_tokenizer = Tokenizer()
 x_tokenizer.fit_on_texts(list(x_train))
 
 # TODO: Возможно, на 4 поменять, если медленно обучаться будет
-thresh = 4
+thresh = 1
 
 cnt = 0
 tot_cnt = 0
@@ -177,7 +173,7 @@ print(x_voc)
 y_tokenizer = Tokenizer()
 y_tokenizer.fit_on_texts(list(y_train))
 
-thresh = 6
+thresh = 1
 
 cnt = 0
 tot_cnt = 0
@@ -205,7 +201,7 @@ y_val = pad_sequences(y_val_seq, maxlen=max_summary_len, padding='post')
 
 y_voc = y_tokenizer.num_words +1
 
-print((y_tokenizer.word_counts['SOL'], len(y_tr)))
+print((y_tokenizer.word_counts['sol'], len(y_tr)))
 
 
 # Удаление предложений, содержащих только SOL и EOL
@@ -235,3 +231,156 @@ y_val = np.delete(y_val, ind, axis=0)
 x_val = np.delete(x_val, ind, axis=0)
 
 # Построение модели
+from keras import backend as K
+K.clear_session()
+
+latent_dim = 300
+embedding_dim = 100
+
+# Кодировщик
+encoder_inputs = Input(shape=(max_text_len,))
+
+# embedding layer
+enc_emb = Embedding(x_voc, embedding_dim, trainable=True)(encoder_inputs)
+
+# encoder lstm 1
+encoder_lstm1 = LSTM(latent_dim, return_sequences=True, return_state=True, dropout=0.4, recurrent_dropout=0.4)
+encoder_output1, state_h1, state_c1 = encoder_lstm1(enc_emb)
+
+# encoder lstm 2
+encoder_lstm2 = LSTM(latent_dim, return_sequences=True, return_state=True, dropout=0.4, recurrent_dropout=0.4)
+encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1)
+
+# encoder lstm 3
+encoder_lstm3 = LSTM(latent_dim, return_state=True, return_sequences=True, dropout=0.4, recurrent_dropout=0.4)
+encoder_outputs, state_h, state_c = encoder_lstm3(encoder_output2)
+
+# Настройка декодировщика
+decoder_inputs = Input(shape=(None,))
+
+# embedding layer
+dec_emb_layer = Embedding(y_voc, embedding_dim, trainable=True)
+dec_emb = dec_emb_layer(decoder_inputs)
+
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True, dropout=0.4, recurrent_dropout=0.2)
+decoder_outputs,decoder_fwd_state, decoder_back_state = decoder_lstm(dec_emb, initial_state=[state_h, state_c])
+
+# слой Attention
+attn_layer = AttentionLayer(name='attention_layer')
+attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs])
+
+# Соединяем слой внимания и декодировщик LSTM
+decoder_concat_input = Concatenate(axis=-1, name='concat_layer')([decoder_outputs, attn_out])
+
+# dense слой
+decoder_dense = TimeDistributed(Dense(y_voc, activation='softmax'))
+decoder_outputs = decoder_dense(decoder_concat_input)
+
+# Определение модели
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+model.summary()
+
+model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy')
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,patience=2)
+
+del data_train
+del data_val
+del data_test
+del summary_word_count
+
+del x_tr_seq
+
+del y_tr_seq
+del y_val_seq
+del x_val_seq
+
+history=model.fit([x_tr,y_tr[:,:-1]], y_tr.reshape(y_tr.shape[0],y_tr.shape[1], 1)[:,1:] ,epochs=4,callbacks=[es],batch_size=128, validation_data=([x_val,y_val[:,:-1]], y_val.reshape(y_val.shape[0],y_val.shape[1], 1)[:,1:]))
+
+
+from matplotlib import pyplot
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
+
+
+reverse_target_word_index = y_tokenizer.index_word
+reverse_source_word_index = x_tokenizer.index_word
+target_word_index = y_tokenizer.word_index
+
+encoder_model = Model(inputs=encoder_inputs,outputs=[encoder_outputs, state_h, state_c])
+
+decoder_state_input_h = Input(shape=(latent_dim,))
+decoder_state_input_c = Input(shape=(latent_dim,))
+decoder_hidden_state_input = Input(shape=(max_text_len,latent_dim))
+
+dec_emb2= dec_emb_layer(decoder_inputs)
+
+decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2, initial_state=[decoder_state_input_h, decoder_state_input_c])
+
+
+attn_out_inf, attn_states_inf = attn_layer([decoder_hidden_state_input, decoder_outputs2])
+decoder_inf_concat = Concatenate(axis=-1, name='concat')([decoder_outputs2, attn_out_inf])
+
+
+decoder_outputs2 = decoder_dense(decoder_inf_concat)
+
+
+decoder_model = Model(
+    [decoder_inputs] + [decoder_hidden_state_input,decoder_state_input_h, decoder_state_input_c],
+    [decoder_outputs2] + [state_h2, state_c2])
+
+
+def decode_sequence(input_seq):
+    e_out, e_h, e_c = encoder_model.predict(input_seq)
+
+    target_seq = np.zeros((1, 1))
+
+    target_seq[0, 0] = target_word_index['sol']
+
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+
+        output_tokens, h, c = decoder_model.predict([target_seq] + [e_out, e_h, e_c])
+
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_token = reverse_target_word_index[sampled_token_index]
+
+        if sampled_token != 'eol':
+            decoded_sentence += ' ' + sampled_token
+
+        if sampled_token == 'eol' or len(decoded_sentence.split()) >= (max_summary_len - 1):
+            stop_condition = True
+
+        target_seq = np.zeros((1, 1))
+        target_seq[0, 0] = sampled_token_index
+
+        e_h, e_c = h, c
+
+    return decoded_sentence
+
+
+def seq2summary(input_seq):
+    newString=''
+    for i in input_seq:
+        if((i!=0 and i!=target_word_index['sol']) and i!=target_word_index['eol']):
+            newString=newString+reverse_target_word_index[i]+' '
+    return newString
+
+
+def seq2text(input_seq):
+    newString=''
+    for i in input_seq:
+        if(i!=0):
+            newString=newString+reverse_source_word_index[i]+' '
+    return newString
+
+
+for i in range(0,100):
+    print("Review:",seq2text(x_tr[i]))
+    print("Original summary:",seq2summary(y_tr[i]))
+    print("Predicted summary:",decode_sequence(x_tr[i].reshape(1,max_text_len)))
+    print("\n")
